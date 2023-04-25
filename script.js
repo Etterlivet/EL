1f9bef622bc067860fa84cca647115de569871e8
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2023-04-26 00:33:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-require = require("esm")(module/*, options*/)
+
 
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js'
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js'
